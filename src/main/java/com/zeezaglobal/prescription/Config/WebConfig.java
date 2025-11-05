a2021ba600514dfd8f02a62e3e09ac80a1f7898d
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2025-11-05 12:02:34 -0600

    updated coars

--- src/main/java/com/zeezaglobal/prescription/Config/WebConfig.java
+++ src/main/java/com/zeezaglobal/prescription/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allow CORS for React app on localhost:3000
         registry.addMapping("/**")
-                .allowedOrigins("http://147.93.114.66:8081", "http://localhost:5173", "http://127.0.0.1:9090","https://indigorx.me")  // Add your React app URL
+                .allowedOrigins("http://147.93.114.66:8081", "http://localhost:3000", "http://127.0.0.1:9090","https://indigorx.me")  // Add your React app URL
                 .allowedMethods("GET", "POST", "DELETE");
     }
 }
